54ee460b7f2500665bd25b1046d13a89f9e6c319
King Nak <philip.masser@gmail.com>
2020-10-03 03:05:07 +0200

    Updated release bat

--- release.bat
+++ release.bat
@@ -6,11 +6,7 @@ mkdir distr\Data
 copy src\KN_KeepThemClean.esp distr\Data
 
 mkdir "distr\Data\scripts"
-copy src\scripts\AARiftPoolActivator.pex "distr\Data\scripts"
-copy src\scripts\KN_KTC_BathPlayerScript.pex "distr\Data\scripts"
-copy src\scripts\KN_KTC_BathQuest.pex "distr\Data\scripts"
-copy src\scripts\KN_KTC_MaintenanceScript.pex "distr\Data\scripts"
-copy src\scripts\KN_KTC_UpdateQuest.pex "distr\Data\scripts"
+copy src\scripts\*.pex "distr\Data\scripts"
 
 cd distr
 "C:\Program Files\7-Zip\7z.exe" a "KN Keep Them Clean 1.0.7z" *
